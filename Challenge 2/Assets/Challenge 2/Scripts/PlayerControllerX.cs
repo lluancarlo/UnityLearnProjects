14620a2a12f6dbe96e928e02e4bbae0372936ee4
lluancarlo <luanveloz@gmail.com>
2024-04-08 22:30:55 +0200

    feat: finished challenge 2

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float SpacePressRate = 0.5f;
+    private float lastSpacePress;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time > (lastSpacePress + SpacePressRate) && Input.GetKeyDown(KeyCode.Space))
+        {
+						lastSpacePress = Time.time;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    public float spawnLimitXLeft = -22;
+    public float spawnLimitXRight = 7;
+    public float spawnPosY = 30;
+
+    public float startDelay = 1.0f;
+    public float spawnIntervalMin = 3.0f;
+    public float spawnIntervalMax = 5.0f;
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(spawnIntervalMin, spawnIntervalMax));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        index = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
+    }
+
+}
