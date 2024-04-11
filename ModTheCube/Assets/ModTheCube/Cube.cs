55a59163a1dced51c13c89164855d9d371e23d81
lluancarlo <luanveloz@gmail.com>
2024-04-11 21:13:16 +0200

    feat: added new ModTheCube project

---
+++ ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 2);
+        transform.localScale = Vector3.one * 1.3f;
+        Material material = Renderer.material;
+        material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 7.0f * Time.deltaTime, 4.0f * Time.deltaTime);
+        transform.localScale = Vector3.one * (float)Math.Sin(Time.time) + Vector3.one * 2f;
+    }
+}
