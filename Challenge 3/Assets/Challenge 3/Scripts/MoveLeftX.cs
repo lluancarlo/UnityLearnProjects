536bdedd30f1dd0a463c7f88802f74bf2499b56e
lluancarlo <luanveloz@gmail.com>
2024-04-14 01:25:48 +0200

    feat: added challenge 3 project

---
+++ Challenge 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+	public float speed;
+	private PlayerControllerX playerControllerScript;
+	private float leftBound = -10;
+
+	void Start()
+	{
+		playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+	}
+
+	void Update()
+	{
+		if (!playerControllerScript.gameOver)
+		{
+			transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+		}
+
+		if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
