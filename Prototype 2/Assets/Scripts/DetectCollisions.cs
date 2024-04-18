cff95ebbe237b1fced7892563cfec86ba6e64a76
lluancarlo <luanveloz@gmail.com>
2024-04-18 16:09:28 +0200

    feat: improve performance using object pooling

--- Prototype 2/Assets/Scripts/DetectCollisions.cs
+++ Prototype 2/Assets/Scripts/DetectCollisions.cs
@@ -1,10 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
-	private void OnTriggerEnter(Collider other)
-	{
-		Destroy(gameObject);
-		Destroy(other.gameObject);
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(other.gameObject); 
+
+        // Just deactivate the food and destroy the animal
+        other.gameObject.SetActive(false);
+    }
+
 }
